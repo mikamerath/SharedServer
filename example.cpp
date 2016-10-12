a9e74fafb04e9972491ce2aa2d35c5c9f010dcd5
michaelkamerath <mkamerath@gmail.com>
2016-10-12 16:09:16 -0600

    Updating files to include information about externpro and adding example code to test its ability to find boost

---
+++ example.cpp
@@ -0,0 +1,23 @@
+//Boost Includes
+#include <boost/algorithm/string.hpp>
+
+//Standard Includes
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main()
+{
+  std::string line("Test\tOtherTest\tAnotherTest");
+  std::vector<std::string> stringVector;
+  boost::split(stringVector, line, boost::is_any_of("\t"));
+
+  std::cout << "The size of the vector is: " << stringVector.size() << std::endl;
+  for (std::string s : stringVector)
+  {
+    std::cout << s << std::endl;
+  }
+
+  std::cin.get();
+  return 0;
+}
