ced2bfdcceb7ef63c77a7707ecb1fcdd960b6779
katiesweet <katie.sweet@hotmail.com>
2016-10-20 22:46:47 -0600

    Add initializeDeck function

--- PlayerAPI/Card.cpp
+++ PlayerAPI/Card.cpp
@@ -8,95 +8,86 @@
  * Suit is an enum {HEARTS}
  *
  * The constuctor accepts the suit enum and one integer.
- * The suit enum (HHEARTS, SPADES, CLUBS, DIAMONDS, JOKER) determines the suit.
+ * The suit enum (HEARTS, SPADES, CLUBS, DIAMONDS, JOKER) determines the suit.
  * The integer (1-13) determines the value. Value is on a 1 based index for easier
  * scoring.
  *
  */
 
 #include <iostream>
+#include <vector>
+#include <random>
+#include <iterator>
+
 
 enum Suit { HEARTS, SPADES, CLUBS, DIAMONDS, UNDEFINED };
 
 class Card {
-  //	card(){} Commented out the default constructor as an empty card object
-  //is meaningless.
-  ~card() {}
-  Suit suit;
-  int value;
-  int id;
-  static int numCardsConstructed;
-  
-  bool operator<(const Card& a) const
-  {
-      return id < a.id; 
-  }
-
-  card(Suit su){
-      id = ++numCardsConstructed;
-      suit = su;
-      if (su == UNDEFINED){
-          value =-1;
-      }
-      else{
-        std::cout << "Error. Cannot instantiate a card without a value." << std::endl;
-        throw(2);
-      }
-  }
-
-  card(Suit su, int val) {
-      id = ++numCardsConstructed;
-      // Assign suit
-      suit = su;
-
-      // Assign Value
-      if (su == JOKER) {
-        value = -1; // a card of type JOKER will take any value.
-      }
-      else if (val < 1 || val > 13) {
-        // Values are from 1 to 13 corrosponding from Ace - King.
-        std::cout << "Error: Tried to instantiate a card of undefined value"
-                  << std::endl;
-        throw(2);
-      }
-
-      else {
-        value = val;
-      }
-  }
-
-  int getSuit() { return suit; }
-  int getValue() { return value; }
+private:
+    Suit suit;
+    int value;
+    
+    
+public:
+    ~Card() {}
+    Card(Suit su){
+        suit = su;
+        if (su == UNDEFINED){
+            value =-1;
+        }
+        else{
+            std::cout << "Error. Cannot instantiate a card without a value." << std::endl;
+            throw(2);
+        }
+    }
+    Card(Suit su, int val) {
+        // Assign suit
+        suit = su;
+        
+        // Assign Value
+        if (su == UNDEFINED) {
+            value = -1; // a card of type UNDEFINED will take any value.
+        }
+        else if (val < 1 || val > 13) {
+            // Values are from 1 to 13 corrosponding from Ace - King.
+            std::cout << "Error: Tried to instantiate a card of undefined value"
+            << std::endl;
+            throw(2);
+        }
+        
+        else {
+            value = val;
+        }
+    }
+    
+    Suit getSuit() const { return suit; }
+    int getValue() const { return value; }
+    
+    bool operator<(Card const & b) const {
+        if (suit < b.getSuit()){
+            return true;
+        }
+        else if (suit > b.getSuit()){
+            return false;
+        }
+        else{
+            return value < b.getValue();
+        }
+    }
 };
 
-// bool testCardStruct() {
-//   bool pass = false;
-//   // Create all Cards in a 52 card deck. if getSuit() and getValue() return the
-//   // correct value, the test passes.
-//   for (int i = 0; i < 4; i++) {
-//     for (int j = 1; j < 14; j++) {
-//       card c(i, j);
-//       if (c.getSuit() == i && c.getValue() == j) {
-//         pass = true;
-//       } else {
-//         pass = false;
-//       }
-//     }
-//   }
-//
-//   // Create a joker. Once again, if getSuit() and getValue() return the correct
-//   // value, the test passes.
-//   card j(
-//       4,
-//       100); // purposefully demonstrate any value is assigned to a joker card.
-//   if (j.getSuit() == 4 && j.getValue() == 100) {
-//     pass = true;
-//   } else {
-//     pass = false;
-//   }
-//   // card c(-1, 15);// spits out an error;
-//   // card d("string", "string"); //Won't compile.
-//   // card e('c', 'c');// spits out an error;
-//   // card f(2.3, 8.7);// works if you really want to use a float or somethihng.
-//   return pass;
-// }
+
+std::vector<Card> initializeDeck(){
+    std::vector<Card> deck;
+    deck.reserve(52);
+    std::vector<Suit> suits = {HEARTS, SPADES, CLUBS, DIAMONDS};
+    for (auto && suit: suits){
+        for (int i=1; i<14; i++){
+            deck.push_back(Card(suit, i));
+        }
+    }
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::shuffle(deck.begin(), deck.end(), generator);
+    return deck;
+}
