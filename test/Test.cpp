ad6e41a13474d04edf1022fecbc2436ff3760ed0
michaelkamerath <mkamerath@gmail.com>
2016-10-26 19:27:53 -0600

    Add unit tests (#14)
    * Adding Functionality to create and build unit tests
    
    * Fixing boost not working when unit test framework was added
    
    * Allowing for static libraries rather than dynamic libraries for boost
    
    * Fixing linking bugs

---
+++ test/Test.cpp
@@ -0,0 +1,12 @@
+/*This file is where all unit testing will be going. All you need to do
+ *is include the appropriate header files and then perform your test.
+ */
+
+//Boost Includes
+#include <boost/test/unit_test.hpp>
+
+int main()
+{
+
+  return 0;
+}
