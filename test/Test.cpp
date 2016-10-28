753780d16c792b52034dde54966d8f8f3c8383cc
michaelkamerath <mkamerath@gmail.com>
2016-10-28 12:43:04 -0600

    Updating testing framework with examples

--- test/Test.cpp
+++ test/Test.cpp
@@ -2,11 +2,21 @@
  *is include the appropriate header files and then perform your test.
  */
 
-//Boost Includes
+#define BOOST_TEST_MODULE const string test;
+
+// Boost Includes
 #include <boost/test/unit_test.hpp>
 
-int main()
+BOOST_AUTO_TEST_CASE(cardInitialization)
 {
-
-  return 0;
+  BOOST_CHECK_EQUAL(0, 0);
+  BOOST_CHECK_EQUAL(20, 20);
 }
+
+BOOST_AUTO_TEST_CASE(otherTest)
+{
+  BOOST_CHECK_EQUAL(10, 10);
+  BOOST_CHECK_EQUAL(5, 5);
+}
+
+//EOF
