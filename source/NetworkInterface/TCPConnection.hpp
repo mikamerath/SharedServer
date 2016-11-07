05b56413a72febd8b80375f47a6b9402c0c79920
droidkfx <kam157190@gmail.com>
2016-11-07 04:22:24 -0700

    hashed out TCPConnection

--- source/NetworkInterface/TCPConnection.hpp
+++ source/NetworkInterface/TCPConnection.hpp
@@ -1,67 +1,68 @@
 #ifndef TCP_CONNECTION
 #define TCP_CONNECTION
 
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/asio.hpp>
+#include <iostream>
+#include <exception>
+#include <array>
+#include <boost\asio.hpp>
+#include <boost\enable_shared_from_this.hpp>
 
-using boost::asio::ip::tcp;
+/* KNOWN ISSUES
 
+- when closing a connection the client side will fall through and cause infinite read 
+operation if read was called during the loss of communication. This renders the object
+unuseable and will stall the using application.
+
+- there is no error checking once read or write have been called and entered the try block
+this causes undesired effects, see above.
+
+*/
+
+
+/*
+A wrapper for a basic TCP connection. Includes methods for connection and reception
+of connections.
+*/
 class TCPConnection
   : public boost::enable_shared_from_this<TCPConnection>
 {
 public:
+  // Allows for ease of creation in other code. 
   typedef boost::shared_ptr<TCPConnection> pointer;
+  // Factory createor method.
+  static pointer create(boost::asio::io_service& ioService, int port = 0);
 
-  static pointer create(boost::asio::io_service& io_service)
-  {
-    return pointer(new TCPConnection(io_service));
-  }
+  // Attempts to connect to the given host and port
+  void connect(const char* host, const char* port);
+  // Waits for a connection to be requested, fills that request and then returns
+  void accept();
+  // closes the exsisting connection
+  void close();
 
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
+  // Read from the connection until a '/n' is found
+  std::string read();
+  // Write to the connection. Automatically appends a '/n' to the end of the message.
+  void write(std::string msg);
 
-  ip::basic_endpoint<ip::tcp> remoteEndpoint() 
-  {
-    return socket_.remote_endpoint();
-  }
-
-  ip::basic_endpoint<ip::tcp> localEndpoint()
-  {
-    return socket_.local_endpoint();
-  }
-
-  void connect(boost::asio::ip::basic_endpoint<boost::asio::ip::tcp> ep) {
-    socket_.connect(ep);
-  }
-
-  void send(std::string msg) {
-    
-  }
-
-  void start()
-  {
-    /*message_ = make_daytime_string();
-
-    boost::asio::async_write(socket_, boost::asio::buffer(message_),
-      boost::bind(&TCPConnection::handle_write, shared_from_this(),
-        boost::asio::placeholders::error,
-        boost::asio::placeholders::bytes_transferred));*/
-  }
+  // returns the current active port
+  const char* getPort();
+  //boost::asio::ip::tcp::socket getSocket();
 
 private:
-  TCPConnection(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
+  // Constructor used by the create method
+  TCPConnection(boost::asio::io_service& ioService, int port);
 
-  void handle_write(const boost::system::error_code& /*error*/,
-    size_t /*bytes_transferred*/)
-  {
-  }
-
-  tcp::socket socket_;
-  std::string message_;
+  // flag indicating if a conneciton is active
+  bool connected;
+  // flag indicating if the object is waiting for a connection request
+  bool accepting;
+  // the socket to perform communication on
+  boost::asio::ip::tcp::socket socket;
+  // The resolver to be used for attempting to connect
+  boost::asio::ip::tcp::resolver resolver;
+  // The acceptor to be used to wait for a connection
+  boost::asio::ip::tcp::acceptor acceptor;
 };
-#endif // !TCP_CONNECTION
+
+
+#endif // TCP_CONNECTION
