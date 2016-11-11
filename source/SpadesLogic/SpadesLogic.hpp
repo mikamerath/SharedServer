ff54b27475d8496978bc498e4671dd465045d972
= <=>
2016-11-11 12:22:36 -0700

    clang format

--- source/SpadesLogic/SpadesLogic.hpp
+++ source/SpadesLogic/SpadesLogic.hpp
@@ -8,22 +8,22 @@
 class Spades : public Game
 {
 public:
-	void setDeck();
-	void printPlayerHands();
-	void start();
-	void getBids();
-	void beginRound(int);
-	void startTrick();
-	bool validMove(std::vector<Card>, int, Suit&, int);
-	int getTrickWinner(std::vector<Card>, int);
-	int getNextPlayer(int);
-	void score();
-	void recordMove(std::vector<Card>);
-	Spades(std::vector<Player>);
-	~Spades() {}
+  void setDeck();
+  void printPlayerHands();
+  void start();
+  void getBids();
+  void beginRound(int);
+  void startTrick();
+  bool validMove(std::vector<Card>, int, Suit&, int);
+  int getTrickWinner(std::vector<Card>, int);
+  int getNextPlayer(int);
+  void score();
+  void recordMove(std::vector<Card>);
+  Spades(std::vector<Player>);
+  ~Spades() {}
 private:
-	int starter;
-	bool spadesBroken;
+  int starter;
+  bool spadesBroken;
 };
 
 #endif
