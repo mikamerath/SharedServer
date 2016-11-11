3c6e03080e21e7b01b85850514f9699b996b4fe3
dmbrowning21 <dave.browning21@aggiemail.usu.edu>
2016-11-11 08:59:50 -0700

    Split into hpp and cpp files

--- source/SpadesLogic/SpadesLogic.cpp
+++ source/SpadesLogic/SpadesLogic.cpp
@@ -1,27 +1,4 @@
-#include "Game.hpp"
-#include <iostream>
-#include <vector>
-
-class Spades : public Game
-{
-public:
-  void setDeck();
-  void printPlayerHands();
-  void start();
-  void getBids();
-  void beginRound(int);
-  void startTrick();
-  bool validMove(std::vector<Card>, int, Suit&, int);
-  int getTrickWinner(std::vector<Card>, int);
-  int getNextPlayer(int);
-  void score();
-  void recordMove(std::vector<Card>);
-  Spades(std::vector<Player>);
-  ~Spades() {}
-private:
-  int starter;
-  bool spadesBroken;
-};
+#include "SpadesLogic.hpp"
 
 int Spades::getNextPlayer(int plId)
 {
---
+++ source/SpadesLogic/SpadesLogic.hpp
@@ -0,0 +1,29 @@
+#ifndef SPADESLOGIC_HPP
+#define SPADESLOGIC_HPP
+
+#include "Game.hpp"
+#include <iostream>
+#include <vector>
+
+class Spades : public Game
+{
+public:
+	void setDeck();
+	void printPlayerHands();
+	void start();
+	void getBids();
+	void beginRound(int);
+	void startTrick();
+	bool validMove(std::vector<Card>, int, Suit&, int);
+	int getTrickWinner(std::vector<Card>, int);
+	int getNextPlayer(int);
+	void score();
+	void recordMove(std::vector<Card>);
+	Spades(std::vector<Player>);
+	~Spades() {}
+private:
+	int starter;
+	bool spadesBroken;
+};
+
+#endiff
