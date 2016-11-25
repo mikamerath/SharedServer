679b9b541ef64e65f13c9d39e48b672771ef00a1
droidkfx <kam157190@gmail.com>
2016-11-25 07:43:21 -0700

    comment client

--- source/NetworkInterface/mockClient.cpp
+++ source/NetworkInterface/mockClient.cpp
@@ -5,16 +5,17 @@
 int main() {
   io_service service;
   ClientNetworkInterface NI(0, service, std::cout);
-  std::cout << "Hello World" << std::endl;
-  NI.connect("127.0.0.1", 12000);
-  if (NI.isConnected()) {
+  NI.connect("127.0.0.1", 12000); // the port wont change, but the IP will be the Ip of 
+                                  // the server.
+
+  if (NI.isConnected()) { // start the client in online mode
     std::string msg = "";
     while (msg != "EXIT") {
       std::getline(std::cin, msg);
       NI.send(msg);
     }
   }
-  else {
+  else { // start in offline mode
     std::cout << "Could not connect... press enter to quit";
     std::cin.get();
   }
