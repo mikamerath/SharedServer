a1e59c82a51072b3b889d6f9b2f9c601354f4fbe
droidkfx <kam157190@gmail.com>
2016-11-25 08:37:31 -0700

    trying...

--- source/Server.cpp
+++ source/Server.cpp
@@ -1,1 +1,13 @@
 #include"Server.h"
+#include "Server.hpp"
+
+void Server::addPlayer(std::shared_ptr<Player> p)
+{
+  std::cout << "Player Added... " << *p << std::endl;
+  players.emplace_back(p);
+  p->readMessage();
+}
+
+void Server::run()
+{
+}
