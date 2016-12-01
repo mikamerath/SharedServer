2190371fcb273c66da458a59ee7e9f1a4dd78261
davidofthehelmickclan <davidofthehelmickclan@yahoo.com>
2016-12-01 12:24:19 -0700

    Update Game.hpp

--- source/PlayerAPI/Game.hpp
+++ source/PlayerAPI/Game.hpp
@@ -68,43 +68,4 @@ public:
   std::vector<Card> getDeck() { return deck; }
 };
 
-class Message
-{
-	friend class boost::serialization::access;
-
-public:
-  State s;
-  bool turn;
-  std::vector<Card> field;
-  std::vector<int> handSizes;
-  std::vector<Card> playerHand;
-  bool deckEmpty;
-  
-  Message()
-  {
-
-  }
-
-  Message(State state, bool t, std::vector<Card> f, std::vector<int> h, std::vector<Card> p, bool d)
-  {
-    s = state;
-    turn = t;
-    field = f;
-    handSizes = h;
-    playerHand = p;
-    deckEmpty = d;
-  }
-  
-  template <class Archive>
-  inline void serialize(Archive& ar, const unsigned int version)
-  {
-    ar& s;
-    ar& turn;
-    ar& field;
-    ar& handSizes;
-    ar& playerHand;
-    ar& deckEmpty;
-  }
-};
-
 #endif
